e10017ef1c869198a1f70ffb9db0bbf1cb1c05ab
endersonriera0-lang <endersonriera0@gmail.com>
2026-03-10 11:11:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, render_template_string
 from datetime import datetime, timedelta
 import re
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -330,4 +331,5 @@ def webhook():
         return {"status": "error", "msg": str(e)}, 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
